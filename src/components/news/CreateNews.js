d6cdcfc034e6bed06b28e197b9115b48a6cf3275
Bonaventure <bonarhyme@gmail.com>
2021-08-19 20:56:03 +0100

    Added preview for multiple images using class based component

--- src/components/news/CreateNews.js
+++ src/components/news/CreateNews.js
@@ -1,79 +1,63 @@
-import React, { useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
-import { FaPlus } from "react-icons/fa";
-import { useSelector } from "react-redux";
-import { LinkContainer } from "react-router-bootstrap";
+import React, { Component } from "react";
 
-import MyButton from "../buttons/MyButton";
-import Loader from "../Loader";
-import Message from "../Message";
+export default class MultipleImageUploadComponent extends Component {
+  fileObj = [];
+  fileArray = [];
 
-const CreateNews = () => {
-  const [image, setImage] = useState("");
-
-  const { loading, error, success, serverReply } = useSelector(
-    (state) => state.newsCreate
-  );
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Worki!!!!!");
-  };
-  const handleImages = async (e) => {
-    // console.log(e.target.files);
-
-    const file = e.target.files[0];
-    var reader = new FileReader();
-    var url = reader.readAsDataURL(file);
-
-    reader.onloadend = function (e) {
-      setImage(reader.result);
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: [null],
     };
-  };
-  return (
-    <main className="register">
-      <Container fluid className="justify-content-center align-items-center">
-        <Row className="justify-content-center ">
-          <Col className="py-5 my-5 register-container box-shadow-white" md={6}>
-            <h2 className="main-header text-center">Create Games</h2>
-            <Form onSubmit={handleSubmit}>
-              {error && <Message variant="danger">{error}</Message>}
-              {loading && <Loader />}
-              {image && (
-                <img
-                  src={image}
-                  alt=""
-                  height="200"
-                  width="200"
-                  style={{ objectFit: "contain" }}
-                />
-              )}
-              <Form.Group controlId="formBasicImages">
-                <Form.File
-                  accept="/image/*"
-                  multiple={true}
-                  onChange={(e) => handleImages(e)}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicTitle">
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="News Title"
-                  className="about-form"
-                  //   onChange={(e) => setUsername(e.target.value)}
-                  required
-                  autoFocus
-                />
-              </Form.Group>
+    this.uploadMultipleFiles = this.uploadMultipleFiles.bind(this);
+    this.uploadFiles = this.uploadFiles.bind(this);
+  }
 
-              <MyButton />
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </main>
-  );
-};
+  uploadMultipleFiles(e) {
+    this.fileObj.push(e.target.files);
+    for (let i = 0; i < this.fileObj[0].length; i++) {
+      this.fileArray.push(URL.createObjectURL(this.fileObj[0][i]));
+    }
+    this.setState({ file: this.fileArray });
+  }
 
-export default CreateNews;
+  uploadFiles(e) {
+    e.preventDefault();
+    console.log(this.state.file);
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="form-group multi-preview">
+          {(this.fileArray || []).map((url, index) => (
+            <img
+              src={url}
+              key={index}
+              alt=""
+              height="200"
+              width="200"
+              style={{ objectFit: "contain" }}
+            />
+          ))}
+        </div>
+
+        <div className="form-group">
+          <input
+            type="file"
+            className="form-control"
+            onChange={this.uploadMultipleFiles}
+            multiple
+          />
+        </div>
+        <button
+          type="button"
+          className="btn btn-danger btn-block"
+          onClick={this.uploadFiles}
+        >
+          Upload
+        </button>
+      </form>
+    );
+  }
+}
